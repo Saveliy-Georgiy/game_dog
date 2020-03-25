e93f9bedda364b48da78eac9f78a55a4c96909df
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-03-25 21:26:49 +0300

    Updates

---
+++ precache-manifest.5064231025ba6986cb1f193119f1fca1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b059d79e499e6281b90546cc245a7669",
+    "url": "/game_dog/index.html"
+  },
+  {
+    "revision": "0cfc14465f491a744250",
+    "url": "/game_dog/static/css/main.2c7d709b.chunk.css"
+  },
+  {
+    "revision": "8d30f2d2f35217d44a0f",
+    "url": "/game_dog/static/js/2.f6e7e21b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game_dog/static/js/2.f6e7e21b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cfc14465f491a744250",
+    "url": "/game_dog/static/js/main.78d8d434.chunk.js"
+  },
+  {
+    "revision": "6d800778d3d0e9092b83",
+    "url": "/game_dog/static/js/runtime-main.65d27b20.js"
+  },
+  {
+    "revision": "304d75e99fe578911d796a13b9f286ea",
+    "url": "/game_dog/static/media/WordPress.304d75e9.jpg"
+  },
+  {
+    "revision": "7b484a183e3434ee3d15247dd9c3e0ec",
+    "url": "/game_dog/static/media/vystrel-drobovika.7b484a18.mp3"
+  }
+]);
